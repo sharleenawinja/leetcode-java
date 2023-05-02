664a138c06e50897b1fc1ebd9f1e065f49f15e3c
sharleenawinja <awinjasharleen@gmail.com>
2023-05-02 16:33:50 +0300

    using String.format method

--- PhoneNumberFormatter.java
+++ PhoneNumberFormatter.java
@@ -20,7 +20,7 @@ public class PhoneNumberFormatter {
             phoneNumber += numbers[i];
         }
 
-        //add dash
+        //add hyphen
         phoneNumber += "-";
 
         //add remaining numbers
@@ -31,4 +31,14 @@ public class PhoneNumberFormatter {
         //return phone number
         return phoneNumber;
     }
+
+    // using String.format
+    // the string format method is used to format the output string using a specified format string and the given arguments
+    // in this case, the format string contains placeholders for each digit in the phone number, separated by the desired formatting characters(parentheses, spaces, hyphens)
+    // the placeholders are represented by "%d", which is a format specifier for integer values
+    // the values to be inserted in these placeholders are obtained by accessing the corressponding element in the input "numbers" array
+    // the formatted string is returne as the output 
+    public static String createPhoneNumber2(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+      }
 }
