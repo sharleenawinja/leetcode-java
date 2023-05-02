e0a67a4224036d19f0062363d4d109dbf45043d1
sharleenawinja <awinjasharleen@gmail.com>
2023-05-02 11:12:23 +0300

    using logical or operator

--- RockPaperScisssors.java
+++ RockPaperScisssors.java
@@ -48,4 +48,26 @@ public class RockPaperScisssors {
         }
         return "Unknown result";
     }
+
+    // using ternary operator and concatenation
+    public static String rps(String p1, String p2) {
+        if(p1.equals(p2)) return "Draw!";
+        int p = (p1 + p2).equals("rockscissors") || (p1 + p2).equals("paperrock") || (p1 + p2).equals("scissorspaper") ? 1 : 2;
+        return "Player " + p + " won!";
+    }
+
+    //using logical and operator
+    public static String rpsGame(String p1, String p2) {
+        if (p1 == p2) {
+            return "Draw!";
+        } else if (p1.equals("rock") && p2.equals("scisssors")) {
+            return "Player 1 won";
+        } else if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won";
+        } else if(p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won";
+        } else {
+            return "Player 2 won";
+        }
+    }
 }
